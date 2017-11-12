f6c726ac54266c620d2f0decc992a10b9dba2a47
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-11-12 18:13:25 +0530

    Adding File IO Domain

---
+++ src/main/scala/com/asb/cats/par/domains/FileIODomain.scala
@@ -0,0 +1,47 @@
+package com.asb.cats.par.domains
+
+import java.io._
+import java.nio.file.Path
+
+import cats.InjectK
+import cats.free.Free
+import cats.free.Free.inject
+
+object FileIODomain {
+
+  sealed trait FileIO[A]
+
+  case class CreateTempFile(filename: String, ext: String) extends FileIO[Path]
+
+  case class GetFilePath(filename: String) extends FileIO[Path]
+
+  case class GetBufferedReader(path: Path) extends FileIO[BufferedReader]
+
+  case class Close(closeable: Closeable) extends FileIO[Unit]
+
+  case class GetBufferedWriter(path: Path) extends FileIO[BufferedWriter]
+
+
+  class FileIOs[F[_]](implicit I: InjectK[FileIO, F]) {
+
+    def createFilePath(filename: String, ext: String): Free[F, Path] =
+      inject(CreateTempFile(filename, ext))
+
+    def getFilePath(string: String): Free[F, Path] =
+      inject(GetFilePath(string))
+
+    def getBufferedReader(path: Path): Free[F, BufferedReader] =
+      inject(GetBufferedReader(path))
+
+    def close(closeable: Closeable): Free[F, Unit] =
+      inject(Close(closeable))
+
+    def getBufferedWriter(path: Path): Free[F, BufferedWriter] =
+      inject(GetBufferedWriter(path))
+  }
+
+  object FileIOs {
+    implicit def apply[F[_]](implicit I: InjectK[FileIO, F]): FileIOs[F] = new FileIOs[F]
+  }
+
+}
